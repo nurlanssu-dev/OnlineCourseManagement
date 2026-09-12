18ce10f75cd38d15b6a2ff7001a6e09f19bd160b
nurlanssu <nurlanssu@code.edu.az>
2026-09-13 01:38:45 +0400

    add change

--- OnlineCourseManagement/Services/EnrollmentService.cs
+++ OnlineCourseManagement/Services/EnrollmentService.cs
@@ -79,4 +79,94 @@ public class EnrollmentService
             .ThenInclude(e => e.Course)
             .FirstOrDefault(s => s.Id == studentId);
     }
+
+    //GET COURSE STUDENTS
+
+    public Course? GetCourseStudents(int courseId)
+    {
+        return _context.Courses
+            .Include(c => c.Enrollments)
+            .ThenInclude(e => e.Student)
+            .FirstOrDefault(c => c.Id == courseId);
+    }
+
+    // REMOVE ENROLLMENT
+
+    public string RemoveEnrollment(int studentId, int courseId)
+    {
+        Enrollment? enrollment = _context.Enrollments
+            .FirstOrDefault(e => e.StudentId == studentId &&
+                                 e.CourseId == courseId);
+
+        if (enrollment == null)
+        {
+            return "Enrollment not found!";
+        }
+
+        _context.Enrollments.Remove(enrollment);
+        _context.SaveChanges();
+
+        return "Student removed from course successfully!";
+    }
+
+    //COURSE STUDENT COUNT
+
+    public int GetCourseStudentCount(int courseId)
+    {
+        return _context.Enrollments
+            .Count(e => e.CourseId == courseId);
+    }
+
+    //STUDENT COURSE COUNT
+
+    public int GetStudentCourseCount(int studentId)
+    {
+        return _context.Enrollments
+            .Count(e => e.StudentId == studentId);
+    }
+
+    //MOST POPULAR COURSE
+
+    public Course? GetMostPopularCourse()
+    {
+        return _context.Courses
+            .Include(c => c.Enrollments)
+            .OrderByDescending(c => c.Enrollments.Count)
+            .FirstOrDefault();
+    }
+
+    //MOST ACTIVE STUDENT
+
+    public Student? GetMostActiveStudent()
+    {
+        return _context.Students
+            .Include(s => s.Enrollments)
+            .OrderByDescending(s => s.Enrollments.Count)
+            .FirstOrDefault();
+    }
+
+    // STUDENT PAGINATION
+
+    public List<Student> GetStudentsByPage(int pageNumber, int pageSize)
+    {
+        return _context.Students
+            .OrderBy(s => s.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+    }
+
+    // COURSE PAGINATION
+
+    public List<Course> GetCoursesByPage(int pageNumber, int pageSize)
+    {
+        return _context.Courses
+            .OrderBy(c => c.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+    }
+
+
+
 }
